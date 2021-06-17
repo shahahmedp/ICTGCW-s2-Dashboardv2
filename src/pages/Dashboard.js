9ba66c57d768f6f1c311b0d2d2505f0b053d7e25
shahbaaza <shahbaazap@cdac.in>
2021-06-17 14:59:10 +0530

    updated

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -7,23 +7,6 @@ const Dashboard = () => {
             <div className="row">
                 <div className="col-lg-3 col-6">
                     <SmallBox
-                        count={150}
-                        title="New Orders"
-                        type="info"
-                        icon="ion-android-people"
-                        navigateTo="/"
-                    />
-                </div>
-                <div className="col-lg-3 col-6">
-                    <SmallBox
-                        count={53}
-                        title="Bounce Rate"
-                        type="success"
-                        navigateTo="/"
-                    />
-                </div>
-                <div className="col-lg-3 col-6">
-                    <SmallBox
                         count={44}
                         title="User Registrations"
                         type="warning"
